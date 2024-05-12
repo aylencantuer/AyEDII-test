c061b4fe3d721c00e70d5815463160404e183510
aylencantuer <aylencantuer02@gmail.com>
2024-05-12 16:07:38 -0300

    Ahora en Cloud

---
+++ dos.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hola mundo" << endl;   // Castellano
+    cout << "Hello world" << endl;  // Ingles
+    cout << "Hallo welt" << endl;   // Aleman
+    cout << "Ciao mondo" << endl;   // Italiano
+    cout << "Hola mon" << endl;     // Catalan
+    cout << "Salut monde" << endl; // Frances
+}
